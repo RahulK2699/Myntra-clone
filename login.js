409ded7be72f2c4d917d006def8557196ccc12da
rushiap05 <rushiapohare@gmail.com>
2021-08-27 16:36:07 +0530

    newly added link

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 function validate() {
     var num = document.getElementById("numinput").value;
     if(num == 8552891970){
-        window.location.href = "http://www.w3schools.com";
+        window.location.href = "homepage.html";
     }else {
         alert("login Id is incorrect")
     }
