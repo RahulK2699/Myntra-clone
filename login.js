0874827c95f39ae83092ee3a1c33607cc63e84d4
rushiap05 <rushiapohare@gmail.com>
2021-08-27 13:19:33 +0530

    login javascript

---
+++ login.js
@@ -0,0 +1,8 @@
+function validate() {
+    var num = document.getElementById("numinput").value;
+    if(num == 8552891970){
+        window.location.href = "http://www.w3schools.com";
+    }else {
+        alert("login Id is incorrect")
+    }
+}
