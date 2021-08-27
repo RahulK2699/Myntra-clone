e4f14130f3b246e12fa19338a5a1b3c3c66588c0
rushiap05 <rushiapohare@gmail.com>
2021-08-27 20:02:16 +0530

    everone number added

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 function validate() {
     var num = document.getElementById("mobileNum").value;
     console.log(num);
-    if(num === "8552891970"){
+    if(num === "8552891970" || num === "7979739083" || num === "8793635952" || num === "7440786993"){
         window.location.href = "homepage.html";
        
     }else {
